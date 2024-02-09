7db0b3727dd3457477e02dba6e3f22b55a713ac6
nootanlodhi <nootancsm19028@gmail.com>
2024-02-09 13:01:17 +0530

    Implement pokemon task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+
+  const fetchApi = async (name) => {
+    try {
+      const resp = await fetch("https://pokeapi.co/api/v2/pokemon/" + name);
+      const data = await resp.json();
+      console.log(data.moves);
+      setData(data.moves);
+    } catch (error) {
+      console.log(error);
+      setData(null);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (name) {
+        fetchApi(name.toLowerCase());
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [name]);
+
+  console.log(data);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <input value={name} onChange={handleName} />
+      {data !== null ? (
+        data.map((item, index) => {
+          return (
+            <div key={index}>
+              <h1>{item.move.name}</h1>
+            </div>
+          );
+        })
+      ) : (
+        <h2>No data found</h2>
+      )}
+    </>
   );
-}
+};
 
 export default App;
+
+// bulbasaur
+// ivysaur
+// venusaur
+// charmander
+// charmeleon
